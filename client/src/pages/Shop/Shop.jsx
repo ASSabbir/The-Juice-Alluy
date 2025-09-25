aa52331e8088fa3f45b94ebaeaf98889b9685d72
ASSabbir <atiksabbir1125@gmail.com>
2025-09-26 01:38:51 +0600

    connection established with mongodb

--- client/src/pages/Shop/Shop.jsx
+++ client/src/pages/Shop/Shop.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+// import TextField from "./components/textField";
+// import 'h8k-components';
+
+const title = "Text Append";
 const Shop = () => {
-    return (
-        <div>
-            this is shop
-        </div>
-    );
-};
+   const [firstText, setFirstText] = useState("");
+  const [secondText, setSecondText] = useState("");
 
+  return (
+    <div>
+      <h8k-navbar header={title}></h8k-navbar>
+      <div className="layout-row align-items-centre justify-content-center mt-50">
+        <section className="layout-column">
+          {/* First Text */}
+          <div data-testid="first-text" className='bg-green-500'>
+            <input
+            className='bg-yellow-200'
+              labelText="First Text"
+              onChange={(e) => setFirstText(e.target.value)}
+            />
+          </div>
+
+          {/* Second Text */}
+          <div data-testid="second-text">
+            <input
+             className='bg-yellow-200'
+              labelText="Second Text"
+              onChange={(e) => setSecondText(e.target.value)}
+            />
+          </div>
+
+          {/* Final Appended Text */}
+          <label className="mt-50 text-align-center">
+            Appended Text is:
+            <label
+              className="mt-10 finalText"
+              data-testid="final-text"
+            >
+              {firstText} {secondText}
+            </label>
+          </label>
+        </section>
+      </div>
+    </div>
+  );
+}
 export default Shop;
---
+++ server/index.js
@@ -0,0 +1,51 @@
+const express =require('express')
+const cors =require('cors')
+const app=express()
+const port=5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://juice_alluy:0IkRHBBp1ZuH2pGh@cluster0.ohjfkao.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+//  MongoClient 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+    const database=client.db("JuiceAlluy")
+    const coffeesCollections=database.collection("coffees")
+    app.get('/coffee',async(req,res)=>{
+        const coffee=coffeesCollections.find().toArray();
+        res.send(coffee)
+    })
+    
+   
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send("This Is server of The Juice Alluy")
+})
+
+app.listen(port,()=>{
+    console.log('server is running')
+})
