7acfdfaff5e86b5359e2fbf2651a5cfe4d44b40b
ASSabbir <atiksabbir1125@gmail.com>
2025-10-01 01:58:41 +0600

    added json datas

--- client/src/pages/Home/BestProducts.jsx
+++ client/src/pages/Home/BestProducts.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { FiCoffee } from 'react-icons/fi';
 
 const BestProducts = () => {
     return (
-        <div className={`min-h-screen flex  items-center bg-backgrondDark  w-full  bg-center`}>
+        <div className={`min-h-screen    bg-backgrondDark  w-full  bg-center`}>
+            <div className='flex w-full  justify-center items-center gap-2 text-2xl font-urbanist'>
+                <FiCoffee className='mb-1' />
+                <h1 >Best Coffess</h1>
+            </div>
             
         </div>
     );
